fd19e41fa3c7c386377d577125ddca7d08c63ec5
cesarserrano <xcesarx@gmail.com>
2025-08-07 22:35:51 -0300

    Atualização dos arquivos

--- supabase.js
+++ supabase.js
@@ -5,3 +5,4 @@ const SUPABASE_URL = 'https://syqpjvlbarbywshulpdt.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5cXBqdmxiYXJieXdzaHVscGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MzQ4ODEsImV4cCI6MjA3MDAxMDg4MX0.kIpFY_A671sepJbqCzAuNRQXabX5Z4GG9BZitXFuDDM';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+ 
